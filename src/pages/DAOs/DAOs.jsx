32ee304920ac39255f42eef0781a912a4c6e4a7c
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-16 14:06:31 +0530

    DAOs
    daos page update

--- src/pages/DAOs/DAOs.jsx
+++ src/pages/DAOs/DAOs.jsx
@@ -9,34 +9,39 @@ function DAOs() {
         <HeaderContent/>
         <BodyContent>
             <div className='daos'>
-              <div className="Collector">
-                <h2>BookLoversDAO - Collector DAO</h2>
-                <p>Description: BookLoversDAO is a decentralized community of bibliophiles who are passionate about collecting rare, antique, and valuable books. Members pool their resources to acquire these literary treasures, which are then owned collectively by the DAO. Through a democratic voting process, members decide on which books to purchase, how to preserve them, and whether to loan or exhibit them in libraries and museums. This DAO not only preserves important cultural artifacts but also fosters a global network of book lovers who share a commitment to literary preservation.
 
-</p>
+              <div className="para">
+                <img src='3.jpeg'alt=''/>
+                <h2>BookLoversDAO - Collector DAO</h2>
+                <p>Description: BookLoversDAO is a decentralized community of bibliophiles who are passionate about collecting rare, antique, and valuable books. Members pool their resources to acquire these literary treasures, which are then owned collectively by the DAO. Through a democratic voting process, members decide on which books to purchase, how to preserve them, and whether to loan or exhibit them in libraries and museums. This DAO not only preserves important cultural artifacts but also fosters a global network of book lovers who share a commitment to literary preservation.</p>
               </div>
 
-              <div className="Philanthropy">
+              <div className="para">
+              <img src='4.jpeg'alt=''/>
               <h2>Charity Funding Proposal Voting - Philanthropy DAOs</h2>
                 <p>Description: This DAO is designed to democratize philanthropy by allowing members to propose and vote on funding for charitable projects. Members can submit proposals for initiatives that need financial support, such as disaster relief, education programs, or environmental conservation. Each proposal is thoroughly reviewed and voted on by the community, ensuring that only the most impactful and meaningful projects receive funding. This approach ensures transparency in charitable giving and empowers communities to support causes that align with their values.</p>
               </div>
 
-              <div className="Grants">
+              <div className="para">
+              <img src='5.jpg'alt=''/>
               <h2>Community - GrantsDAO</h2>
                 <p>Description: Community GrantsDAO is a decentralized platform that provides financial grants to grassroots projects and local initiatives. Members of the DAO can propose projects that aim to improve their communities, whether it's through building infrastructure, supporting local businesses, or creating educational programs. Proposals are voted on by the community, and approved projects receive funding from the DAO’s treasury. This model empowers local leaders and organizations by giving them direct access to resources, allowing them to drive meaningful change in their communities.</p>
               </div>
 
-              <div className="Social">
+              <div className="para">
+              <img src='6.jpg'alt=''/>
               <h2>CultureConnectDAO - Social DAOs</h2>
                 <p>Description: CultureConnectDAO is a social DAO that brings together individuals from diverse cultural backgrounds to engage in cross-cultural exchanges and collaborative projects. Members participate in virtual events such as language exchanges, cultural storytelling sessions, and global art showcases. The DAO funds initiatives that promote cultural understanding, such as the production of documentaries, art exhibitions, and educational programs. By leveraging the power of decentralized governance, CultureConnectDAO fosters a global community that celebrates diversity and builds bridges between cultures.</p>
               </div>
 
-              <div className="Protocol">
+              <div className="para">
+              <img src='7.jpeg'alt=''/>
               <h2> Governance Mechanism - Protocol DAOs</h2>
                 <p>Description: Protocol DAOs are at the core of decentralized governance in blockchain networks. This DAO is responsible for managing and evolving the governance structure of a specific blockchain protocol. Members, who often hold governance tokens, can propose and vote on changes to the protocol, such as upgrades, parameter adjustments, or new feature implementations. The DAO operates autonomously, with smart contracts enforcing the rules and decisions made by the community. This ensures that the protocol evolves in a way that aligns with the interests of its users and stakeholders.</p>
               </div>
 
-              <div className="Investment">
+              <div className="para">
+              <img src='8.png'alt=''/>
               <h2> RealEstate - InvestmentDAO</h2>
                 <p>Description: RealEstate InvestmentDAO enables members to pool their capital to invest in real estate properties. Through a decentralized platform, members can propose and vote on potential real estate investments, such as residential, commercial, or industrial properties. The DAO manages the acquisition, management, and potential sale of these properties, distributing profits back to its members based on their contribution and participation in the DAO. This model democratizes real estate investment, allowing individuals to access lucrative opportunities that were previously reserved for large investors.
 
