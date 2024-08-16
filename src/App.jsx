a1e4dcb4d6b85b44e5e59bce6ff40d652f8c6c1d
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-08-16 12:50:14 +0530

    update
    home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,20 @@ function App() {
     <>
       <HeaderContent />
       <BodyContent>
-        <div className="home">
-          <img src='2.png' alt='' />
-        </div>
-        <div>
-          <h1>Welcome to the World of DAOs</h1>
-          <p>Explore the future of decentralized organizations where decisions are made by the community, for the community. DAOs (Decentralized Autonomous Organizations) empower individuals to collaborate and govern collectively, all without the need for central authorities.
 
-            Discover how DAOs are reshaping industries, fostering innovation, and building global communities.</p>
+        <div className="row">
+           <div className="left">
+              <img src='2.png' alt='' />
+           </div>
+           <div className='content'>
+              <h1>Welcome to the World of DAOs</h1>
+              <p>Explore the future of decentralized organizations where decisions are made by the community, for the community. DAOs (Decentralized Autonomous Organizations) empower individuals to collaborate and govern collectively, all without the need for central authorities.
+               Discover how DAOs are reshaping industries, fostering innovation, and building global communities.</p>
+           </div>
+
         </div>
+
+
 
 
       </BodyContent>
